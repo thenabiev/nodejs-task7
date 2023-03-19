026c43c2bfe4bad424a2feae35709d87676717cc
thenabiev <sahinnabee@gmail.com>
2023-03-20 02:23:56 +0400

    connected to db and basic crud operations

--- index.js
+++ index.js
@@ -51,8 +51,19 @@ app.get('/add', (req, res)=>{
 });
 
 app.get('/post', (req, res)=>{
+    
 
     res.render('post')
+});
+
+app.get('/post/:id', async (req, res)=>{
+    
+    const post = await Post.findById(req.params.id);
+
+    res.render('post',{
+        post
+    })
+    
 });
 
 
